730346a9b8aaf407c45ad73c21c7004a8835eed6
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-17 17:07:38 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+const API_KEY="42927784-bb2cd81da21eaa5ba65e9bdd5"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/styles.css"
+import App from "./app"
+ReactDOM.render(<AppRouter />, document.getElementById("root"))
