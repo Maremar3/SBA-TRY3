ac4b52bb742adad4f294ed045764dc88f5e1010a
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-18 11:29:56 -0700

    folder created

---
+++ src/componenets/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <div className='nav'>
+      <Link to='/'>
+        <div>Temrture</div>
+      </Link>
+      <Link to='/Humidity'>
+        <div>Humidity</div>
+      </Link>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Humidity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Humidity() {
+  return (
+    <div>
+      HUMIDITY
+    </div>
+  )
+}
+
+export default H
